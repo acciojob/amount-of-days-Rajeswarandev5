3d4f4b72317cde2896727d98a214448552f343a7
Rajeswarandev5 <rajeswaran.dev5@gmail.com>
2026-07-28 20:14:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	if(year % 400){
+		return 366;
+	} else if (year % 100){
+		return 365;
+	} else if (year % 4){
+		return 366;
+	} else {
+		return 365;
+	}
+}
